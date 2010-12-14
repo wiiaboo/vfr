06e4fcb63069b229976fede5620ad9d67ea7f5d6
Ricardo Constantino <wiiaboo@gmail.com>
2010-12-14 16:51:18 +0000

    Actually enable native v1 timecodes parsing
    Add a way to modify the precision of Ts()
    Use only ms precision for --ofps
    Use fractions module in Ts()

--- vfr.py
+++ vfr.py
@@ -13,6 +13,7 @@ try:
 except ImportError:
     chapparseExists = False
 from subprocess import call
+from fractions import Fraction
 
 rat = re.compile('(\d+)(?:/|:)(\d+)')
 v1re = re.compile('# timecode format v1')
@@ -25,7 +26,6 @@ exts = {
 defaultFps = "30000/1001"
 
 # Change the paths here if the programs aren't in your $PATH
-tcConv = r'tcConv'
 mkvmerge = r'mkvmerge'
 
 def main():
@@ -38,13 +38,12 @@ def main():
     p.add_option('--output', '-o', action="store", help='Cut audio from MKVMerge', dest="output")
     p.add_option('--fps', '-f', action="store", help='Frames per second (for cfr input)', dest="fps")
     p.add_option('--ofps', action="store", help='Output frames per second', dest="ofps")
-    p.add_option('--timecodes', '-t', action="store", help='Timecodes file from the vfr video (v1 needs tcConv)', dest="timecodes")
+    p.add_option('--timecodes', '-t', action="store", help='Timecodes file from the vfr video', dest="timecodes")
     p.add_option('--chapters', '-c', action="store", help='Chapters file [.%s/.txt]' % "/.".join(exts.keys()), dest="chapters")
     p.add_option('--qpfile', '-q', action="store", help='QPFile for x264 (frame-accurate only if used with final framecount)', dest="qpfile")
     p.add_option('--verbose', '-v', action="store_true", help='Verbose', dest="verbose")
     p.add_option('--merge', '-m', action="store_true", help='Merge cut files', dest="merge")
     p.add_option('--remove', '-r', action="store_true", help='Remove cut files', dest="remove")
-    p.add_option('--frames', action="store", help='Number of frames for v1 conversion', dest="frames")
     p.add_option('--test', action="store_true", help="Test mode (do not create new files)", dest="test")
     (o, a) = p.parse_args()
 
@@ -117,22 +116,23 @@ def main():
     # trims' offset calculation
     Trims2 = []
     Trims2ts = []
-    tcType = determineFormat(o.timecodes)
     tc = o.timecodes
+    tcType = determineFormat(tc)
     if tcType == 2:
-        nTrims = int(o.frames) if o.frames else int(Trims[-1][1])+2
-        if not os.path.isfile(tc+"v2.txt"):
-            tcConv = call('"%s" "%s" "%s" %d' % (tcConv, tc, tc+"v2.txt", nTrims))
-            if tcConv > 0:
-                sys.exit("Failed to execute tcConv: %d; Please put it in your path" % tcConv)
-        o.timecodes = tc+"v2.txt"
+        if not os.path.isfile(tc[:-3]+"v2.txt"):
+            tc2 = tc[:-3]+"v2.txt"
+            tmp = tc2
+            parseTc(tc,tmp,Trims[-1][1])
+        else:
+            tc2 = tc[:-3]+"v2.txt"
+        tc = tc2
         tcType = 3
 
     for i in range(len(Trims)):
         fn1 = int(Trims[i][0])                     # first frame
-        fn1ts = Ts(fn1,tc,tcType)[0]               # first frame timestamp
+        fn1ts = vTrunc(Ts(fn1,tc,tcType)[0])       # first frame timestamp
         fn2 = int(Trims[i][1])                     # last frame
-        fn2ts = Ts(fn2,tc,tcType)[0]               # last frame timestamp
+        fn2ts = vTrunc(Ts(fn2,tc,tcType)[0])       # last frame timestamp
         fn2tsaud = Ts(fn2+1,tc,tcType)             # last frame timestamp for audio
 
         # calculate offsets for non-continuous trims
@@ -151,10 +151,10 @@ def main():
             # make list with timecodes to cut audio
             audio.append(formatTime(fn1ts))
             if len(fn2tsaud) == 1:
-                audio.append(formatTime(fn2tsaud[0]))
+                audio.append(formatTime(vTrunc(fn2tsaud[0])))
 
         # apply the offset to the trims
-        lastts=fn2tsaud[0]
+        lastts=vTrunc(fn2tsaud[0])
         fn1 -= offset
         fn2 -= offset
         fn1ts -= offsetts
@@ -162,8 +162,8 @@ def main():
 
         # convert fps if --ofps
         if o.ofps and o.timecodes != o.ofps:
-            fn1 = unTs(fn1ts,o.ofps)
-            fn2 = unTs(fn2ts,o.ofps)
+            fn1 = unTs(fn1,tc,o.ofps)
+            fn2 = unTs(fn2,tc,o.ofps)
 
         # add trims and their timestamps to list
         Trims2.append([fn1,fn2])
@@ -215,6 +215,7 @@ def main():
 
         if o.remove:
             remove = ['%s.split-%03d.mka' % (o.output, i) for i in range(1,len(audio)+2)]
+            if tcType == 2: remove.append(tc)
             if o.verbose: print('\nDeleting: %s\n' % ', '.join(remove))
             if not o.test:
                 [os.unlink(i) if os.path.exists(i) else True for i in remove]
@@ -290,39 +291,52 @@ def determineFormat(timecodes):
     elif v2re.match(linecache.getline(timecodes,1)): return 3
     else: return 0
 
-def parseTc( tcfile ):
-    tc = open(tcfile)
-    ret = re.search('# timecode format v(\d)',tcfile.readline())
-    version = ret.group(1) if ret else sys.exit('file is not in a supported format')
-    if version == '1':
-        tclines = tcfile.readlines()
-        tc.close()
-        for line in tclines:
-            ret = re.search('Assume (\d+(\.\d+)?)(?i)',line)
-            assume = ret.group(1) if ret else None
-        if not assume: sys.exit('there is no assumed fps')
-    elif version == '2':
-        tc.close()
-
 def vTrunc(ts):
     """Truncates a ns timestamp to 0.1ms precision"""
     ts = ts / 10**6
     tts = round(ts,1) if round(ts,1) == math.floor(ts*10)/10 else math.ceil(ts*10)/10-0.05
     return int(round(tts*10**6))
 
-def Ts(fn,tc,tcType=1):
+def parseTc(tcfile,tmp,last):
+    tc = open(tcfile)
+    ts = 0
+    ret = re.search('# timecode format (v\d)',tc.readline())
+    version = ret.group(1) if ret else sys.exit('file is not in a supported format')
+    tmp = open(tmp,'w')
+    if version == 'v1':
+        tclines = tc.readlines()
+        tc.close()
+        ret = re.search('Assume (\d+(?:\.\d+)?)(?i)',tclines[0])
+        assume = Fraction(ret.group(1)).limit_denominator(1001) if ret else sys.exit('there is no assumed fps')
+        overrides = [[range(int(i[0]),int(i[1])+1),Fraction(i[2]).limit_denominator(1001)] for i in re.findall('^(\d+),(\d+),(\d+(?:\.\d+)?)$(?m)',''.join(tclines[1:]))] if len(tclines) > 1 else None
+        ret = re.search('^# TDecimate Mode 3:  Last Frame = (\d)$(?m)',''.join(tclines))
+        last = int(ret) if ret else last
+        tmp.write('# timecode format v2\n')
+        for i in range(int(last)+1):
+            fps = assume
+            for r in overrides:
+                if i in r[0]:
+                    fps = r[1]
+            ts += fps.denominator/fps.numerator if i > 0 else 0
+            tmp.write('{}\n'.format(round(vTrunc(int(round(ts,7)*10**9))/10**6,6)))
+        tmp.close()
+    elif version == 'v2':
+        tc.close()
+
+def Ts(fn,tc,tcType=1,timecode_scale=1000):
     """Returns timestamps (in ns) from a frame number and timecodes file."""
+    scale = 10**12 / timecode_scale
     # CFR
     if tcType == 1:
         fps = rat.search(tc).groups() if rat.search(tc) else [re.search('(\d+)',tc).group(0),'1']
-        ts = int(round((10**5 * fn * float(fps[1])) / int(fps[0])))*10**4
-        return [vTrunc(ts),]
+        ts = int(round((scale * fn * float(fps[1])) / int(fps[0])))
+        return [ts,]
     # VFR
-    elif tcType == 3:
+    elif tcType >= 2:
         ts = linecache.getline(tc,fn+2)
         if ts == '':
             lines = 0
-            with open(tc,"r") as file:
+            with open(tc) as file:
                 for line in file:
                     lines += 1
             nLines = math.ceil(lines / 100)
@@ -342,10 +356,13 @@ def Ts(fn,tc,tcType=1):
     else:
         sys.exit("Couldn't get timestamps")
 
-def unTs(ts,fps):
+def unTs(fn,old,new):
     """Returns a frame number from fps and ofps (ConvertFPS)"""
-    ofps = rat.search(fps).groups() if rat.search(fps) else [re.search('(\d+)',fps).group(0),'1']
-    return int(math.floor(ts / 1000.0 / (float(ofps[1]) / int(ofps[0]))))
+    old=Ts(fn,old,1,10**9)[0]
+    ofps = rat.search(new).groups() if rat.search(new) else [re.search('(\d+)',new).group(0),'1']
+    new=old/10**3/(float(ofps[1])/int(ofps[0]))
+    new=new if math.floor(new) == math.floor(abs(new-0.2)) else new-0.2
+    return int(math.floor(new))
 
 def generateChap(start, end, chapter, type):
     """Generates chapters"""
