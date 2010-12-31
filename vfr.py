09abeada43907010deb7d6052504fd0fd4b31833
Ricardo Constantino <wiiaboo@gmail.com>
2010-12-31 17:50:34 +0000

    Fix regression in previous commit

--- vfr.py
+++ vfr.py
@@ -125,8 +125,8 @@ def main():
     if o.ofps and o.timecodes != o.ofps:
         ofps = parse_tc(o.ofps)[0]
         if o.otc:
-        max = convert_fps(int(Trims[-1][1]),tc,ofps)
-        parse_tc(o.ofps,max+2,o.otc)
+            max = convert_fps(int(Trims[-1][1]),tc,ofps)
+            parse_tc(o.ofps,max+2,o.otc)
 
     for i in range(len(Trims)):
         fn1 = int(Trims[i][0])
