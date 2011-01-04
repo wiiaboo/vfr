8351325ad197cd06419b73fdb658a5c21d60ecba
Ricardo Constantino <wiiaboo@gmail.com>
2011-01-04 02:39:02 +0000

    Implemented a basic formatter for amkvc
    Doesn't use vfr.py yet, but other than missing timecodes, should be almost the same as amkvc itself.

--- templates.py
+++ templates.py
@@ -1,7 +1,94 @@
 #!/usr/bin/env python3.1
 
+class Info:
+    editions = 1
+    lang = 'eng'
+    country = 'us'
+    inputfps = [30000,1001]
+    outputfps = [24000,1001]
+    createqpfile = False
+
+class Edition:
+    default = False
+    name = 'Edition'
+    hidden = False
+    ordered = False
+    chapters = {}
+
+class Chapter:
+    name = 'Chapter'
+    chapter = None
+    start = None
+    end = None
+    suid = None
+    hidden = False
+
 def main(args):
-    cfg = parse_amkvc(args[1])
+    cfg, type = parse_amkvc(args[1])
+    format_chapters(cfg, type)
+
+
+def format_chapters(cfg,type):
+    from random import randint
+    from os import linesep as lsp
+
+    uid = randint(10**5,10**6)
+
+    mkv = ['<?xml version="1.0" encoding="UTF-8"?>\n',
+           '<!-- <!DOCTYPE Tags SYSTEM "matroskatags.dtd"> -->\n']
+    mkvchapters = mkv + ['<Chapters>\n']
+    mkvtags = mkv + ['<Tags>\n']
+    
+    for edition_num in cfg['editions']:
+        uid += 1
+        edition = cfg['editions'][edition_num]
+        mkvedition = ['\t<EditionEntry>\n',
+                      '\t\t<EditionFlagHidden>0</EditionFlagHidden>\n',
+                      '\t\t<EditionFlagDefault>%d</EditionFlagDefault>\n' % int(edition['default']),
+                      '\t\t<EditionFlagOrdered>%d</EditionFlagOrdered>\n' % int(edition['ordered']),
+                      '\t\t<EditionUID>%d</EditionUID>\n' % uid]
+        
+        mkvtags += ['\t<Tag>\n',
+                    '\t\t<Targets>\n',
+                    '\t\t\t<EditionUID>%d</EditionUID>\n' % uid,
+                    '\t\t\t<TargetTypeValue>50</TargetTypeValue>\n',
+                    '\t\t</Targets>\n',
+                    '\t\t<Simple>\n',
+                    '\t\t\t<Name>TITLE</Name>\n',
+                    '\t\t\t<String>%s</String>\n' % edition['name'],
+                    '\t\t\t<TagLanguage>%s</TagLanguage>\n' % cfg['lang'],
+                    '\t\t\t<DefaultLanguage>1</DefaultLanguage>\n',
+                    '\t\t</Simple>\n',
+                    '\t</Tag>\n']
+
+        for chapter_num in edition['chapters']:
+            uid += 1
+            chapter = edition['chapters'][chapter_num]
+            mkvchapter = ['\t\t<ChapterAtom>\n',
+                          '\t\t\t<ChapterDisplay>\n',
+                          '\t\t\t\t<ChapterString>%s</ChapterString>\n' % chapter['name'],
+                          '\t\t\t\t<ChapterLanguage>%s</ChapterLanguage>\n' % cfg['lang'],
+                          '\t\t\t\t<ChapterCountry>%s</ChapterCountry>\n' % cfg['country'],
+                          '\t\t\t</ChapterDisplay>\n',
+                          '\t\t\t<ChapterUID>%d</ChapterUID>\n' % uid,
+                          '\t\t\t<ChapterTimeStart>0</ChapterTimeStart>\n',
+                          '\t\t\t<ChapterTimeEnd>0</ChapterTimeEnd>\n',
+                          '\t\t\t<ChapterFlagHidden>0</ChapterFlagHidden>\n',# % chapter['hidden'],
+                          '\t\t\t<ChapterFlagEnabled>1</ChapterFlagEnabled>\n',
+                          '\t\t</ChapterAtom>\n']
+            mkvedition += mkvchapter
+            
+        mkvedition += ['\t</EditionEntry>\n']
+        mkvchapters += mkvedition
+        
+    mkvchapters += ['</Chapters>\n']
+    
+    outf = open('pilas.xml','w')
+    outf.writelines(mkvchapters)
+    
+    outf2 = open('pilatags.xml','w')
+    outf2.writelines(mkvtags)
+
 
 def parse_amkvc(templatefile):
     from configparser import ConfigParser
@@ -15,11 +102,12 @@ def parse_amkvc(templatefile):
         'inputfps': '30',
         'outputfps': '24',
         'createqpfile': '1',
+        'hidden': '0',
         'ordered': '0',
         'default': '0',
         'chapters': '0'
     }
-    
+
     cfg = {}
     
     chre = compile('(\d+)(\w+)')
@@ -61,7 +149,8 @@ def parse_amkvc(templatefile):
             if chaps:
                 cfg['editions'][ed_num]['chapters'][int(chaps.group(1))][chaps.group(2)] = config.get(edition,key)
 
-    return cfg
+    return cfg, 'MKV'
+
 
 if __name__ == '__main__':
     from sys import argv
