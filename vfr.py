wiiaboo/vfr

commit 62fafe3a7ef00c0250cc10137c7dc8a37d4cc177

Remove deprecated use of % operator, fix incorrect use of subprocess.call and subprocess.check_output
You’re supposed to pass a list of parameters to subprocess.call,
not a single string containing a shell command.

test.py was incorrectly using 'python' to execute vfr.py rather than
'python3'. It also used a backslash as path separator, which is not
portable. Fixed.
This commit slightly changes some output of vfr.py, so right now
test.py will assume that the commands failed.

vfr.py
@@ -27,7 +27,7 @@ def main(args):
     p.add_option('--fps', '-f', action="store", help='Frames per second or Timecodes file', dest="fps")
     p.add_option('--ofps', action="store", help='Output frames per second', dest="ofps")
     p.add_option('--timecodes', action="store", help='Output v2 timecodes', dest="otc")
-    p.add_option('--chapters', '-c', action="store", help='Chapters file [.%s/.txt]' % "/.".join(exts.keys()), dest="chapters")
+    p.add_option('--chapters', '-c', action="store", help='Chapters file [.{0}/.txt]'.format("/.".join(exts.keys())), dest="chapters")
     p.add_option('--chnames', '-n', action="store", help='Path to template file for chapter names (utf8 w/o bom)', dest="chnames")
     p.add_option('--template', '-t', action="store", help="Template file for chapters", dest="template")
     p.add_option('--uid', action="store", help="Base UID for --template or --chnames", dest="uid")
@@ -47,7 +47,7 @@ def main(args):
 
     #Determine chapter type
     if o.chapters:
-        chre = compile("\.(%s)$(?i)" % "|".join(exts.keys()))
+        chre = compile("\.({0})$(?i)".format("|".join(exts.keys())))
         ret = chre.search(o.chapters)
         chapter_type = exts[ret.group(1).lower()] if ret else "OGM"
     else:
@@ -60,23 +60,23 @@ def main(args):
 
     if not o.output and o.input:
         ret = splitext(o.input)
-        o.output = '%s.cut.mka' % ret[0]
+        o.output = '{0}.cut.mka'.format(ret[0])
 
     if o.verbose:
-        status =  "Avisynth file:   %s\n" % a[0]
-        status += "Label:           %s\n" % o.label if o.label else ""
-        status += "Audio file:      %s%s\n" % (o.input, "(SBR)" if o.sbr else "") if o.input else ""
-        status += "Cut Audio file:  %s\n" % o.output if o.output else ""
-        status += "Timecodes/FPS:   %s%s\n" % (o.fps," to "+o.ofps if o.ofps else "") if o.ofps != o.fps else ""
-        status += "Output v2 Tc:    %s\n" % o.otc if o.otc else ""
-        status += "Chapters file:   %s%s\n" % (o.chapters," (%s)" % chapter_type if chapter_type else "") if o.chapters else ""
-        status += "Chapter Names:   %s\n" % o.chnames if o.chnames else ""
-        status += "Template file:   %s\n" % o.template if o.template else ""
-        status += "QP file:         %s\n" % o.qpfile if o.qpfile else ""
+        status =  "Avisynth file:   {0}\n".format(a[0])
+        status += "Label:           {0}\n".format(o.label) if o.label else ""
+        status += "Audio file:      {0}{1}\n".format(o.input, "(SBR)" if o.sbr else "") if o.input else ""
+        status += "Cut Audio file:  {0}\n".format(o.output) if o.output else ""
+        status += "Timecodes/FPS:   {0}{1}\n".format(o.fps, " to " + o.ofps if o.ofps else "") if o.ofps != o.fps else ""
+        status += "Output v2 Tc:    {0}\n".format(o.otc) if o.otc else ""
+        status += "Chapters file:   {0}{1}\n".format(o.chapters, " ({0})".format(chapter_type) if chapter_type else "") if o.chapters else ""
+        status += "Chapter Names:   {0}\n".format(o.chnames) if o.chnames else ""
+        status += "Template file:   {0}\n".format(o.template) if o.template else ""
+        status += "QP file:         {0}\n".format(o.qpfile) if o.qpfile else ""
         status += "\n"
-        status += "Merge/Rem files: %s/%s\n" % (o.merge,o.remove) if o.merge or o.remove else ""
-        status += "Verbose:         %s\n" % o.verbose if o.verbose else ""
-        status += "Test Mode:       %s\n" % o.test if o.test else ""
+        status += "Merge/Rem files: {0}/{1}\n".format(o.merge, o.remove) if o.merge or o.remove else ""
+        status += "Verbose:         {0}\n".format(o.verbose) if o.verbose else ""
+        status += "Test Mode:       {0}\n".format(o.test) if o.test else ""
 
         print(status)
 
@@ -85,68 +85,74 @@ def main(args):
 
     nt2 = len(Trims2ts)
     if o.verbose:
-        print('In trims: %s\n' % ', '.join(['(%s,%s)' % (i[0],i[1]) for i in Trims]))
-        print('In timecodes: %s\n' % ', '.join(['(%s,%s)' % (i[0],i[1]) for i in Trimsts]))
-        print('Out trims: %s\n' % ', '.join(['(%s,%s)' % (i[0],i[1]) for i in Trims2]))
-        print('Out timecodes: %s\n' % ', '.join(['(%s,%s)' % (fmt_time(i[0]),fmt_time(i[1])) for i in Trims2ts]))
+        print('In trims: {0}\n'.format(', '.join(['({0},{1})'.format(i[0], i[1]) for i in Trims])))
+        print('In timecodes: {0}\n'.format(', '.join(['({0},{1})'.format(i[0], i[1]) for i in Trimsts])))
+        print('Out trims: {0}\n'.format(', '.join(['({0},{1})'.format(i[0], i[1]) for i in Trims2])))
+        print('Out timecodes: {0}\n'.format(', '.join(['({0},{1})'.format(fmt_time(i[0]),fmt_time(i[1])) for i in Trims2ts])))
 
     # make qpfile
     if o.qpfile and not o.template:
         if not o.test:
             write_qpfile(o.qpfile,Trims2)
-        if o.verbose: print('Writing keyframes to %s\n' % o.qpfile)
+        if o.verbose: print('Writing keyframes to {0}\n'.format(o.qpfile))
 
     # make audio cuts
     if o.input:
         from subprocess import call, check_output
-        
+
         if Trims[0][0] == 0:
             includefirst = True
             audio = audio[1:]
         else:
             includefirst = False
         cuttimes = ','.join(audio)
-        quiet = '' if o.verbose else '-q'
         max_audio = len(audio)+2
-        
+
         # get info from mkvmerge
-        ident = check_output([mkvmerge,"--identify-for-mmg",o.input])
+        ident = check_output([mkvmerge, "--identify-for-mmg", o.input])
         identre = compile("Track ID (\d+): audio( \(AAC\) \[aac_is_sbr:true\])?")
         ret = identre.search(ident.decode()) if ident else None
-        
+
         tid = ret.group(1) if ret else '0'
-        sbr = " --aac-is-sbr 0:1" if o.sbr or ret.group(2) else " --aac-is-sbr 0:0" if o.input.endswith("aac") else ""
+        sbr = "0:1" if o.sbr or ret.group(2) else "0:0" if o.input.endswith("aac") else ""
 
         # determine delay
         delre = compile('DELAY ([-]?\d+)')
         ret = delre.search(o.input)
-        delay = ' --sync %s:%s' % (tid, o.delay if o.delay else ret.group(1)) if o.delay or ret else ''
+        delay = '{0}:{1}'.format(tid, o.delay if o.delay else ret.group(1)) if o.delay or ret else None
 
-        cutCmd = '"%s" -o "%s"%s%s "%s" --split timecodes:%s %s' % (mkvmerge, o.output + '.split.mka', delay, sbr, o.input, cuttimes, quiet)
-        if o.verbose: print('Cutting: %s\n' % cutCmd)
+        cutCmd = [mkvmerge, '-o', o.output + '.split.mka', o.input, '--split', 'timecodes:' + cuttimes]
+        if delay: cutCmd.extend(['--sync', delay])
+        if sbr: cutCmd.extend(['--aac-is-sbr', sbr])
+        if o.verbose: print('Cutting: {0}\n'.format(' '.join(['"{0}"'.format(i) for i in cutCmd])))
+        else: cutCmd.append('-q')
+
         if not o.test:
             cutExec = call(cutCmd)
             if cutExec == 1:
-                print("Mkvmerge exited with warnings: %d" % cutExec)
+                print("Mkvmerge exited with warnings: {0:d}".format(cutExec))
             elif cutExec == 2:
-                exit("Failed to execute mkvmerge: %d" % cutExec)
+                exit("Failed to execute mkvmerge: {0:d}".format(cutExec))
         if o.merge:
             merge = []
             for i in range(1,max_audio):
                 if (includefirst == True and i % 2 != 0) or (includefirst == False and i % 2 == 0):
-                    merge.append('"%s.split-%03d.mka"' % (o.output, i))
-            mergeCmd = '"%s" -o "%s" %s %s' % (mkvmerge,o.output, ' +'.join(merge), quiet)
-            if o.verbose: print('\nMerging: %s\n' % mergeCmd)
+                    merge.append('"{0}.split-{1:3d}.mka"'.format(o.output, i))
+            #mergeCmd = '"%s" -o "%s" %s %s' % (mkvmerge,o.output, ' +'.join(merge), quiet)
+            mergeCmd = [mkvmerge, o.output, ' +'.join(merge)]
+            if o.verbose: print('\nMerging: {0}\n'.format(mergeCmd))
+            else: mergeCmd.append('-q')
+
             if not o.test:
                 mergeExec = call(mergeCmd)
                 if mergeExec == 1:
-                    print("Mkvmerge exited with warnings: %d" % mergeExec)
+                    print("Mkvmerge exited with warnings: {0:d}".format(mergeExec))
                 elif mergeExec == 2:
-                    exit("Failed to execute mkvmerge: %d" % mergeExec)
+                    exit("Failed to execute mkvmerge: {0:d}".format(mergeExec))
 
         if o.remove:
-            remove = ['%s.split-%03d.mka' % (o.output, i) for i in range(1,max_audio)]
-            if o.verbose: print('\nDeleting: %s\n' % ', '.join(remove))
+            remove = ['{0}.split-{1:3d}.mka'.format(o.output, i) for i in range(1,max_audio)]
+            if o.verbose: print('\nDeleting: {0}\n'.format(', '.join(remove)))
             if not o.test:
                 from os import unlink
                 [unlink(i) if isfile(i) else True for i in remove]
@@ -163,7 +169,7 @@ def main(args):
 
     # write chapters
     if chapter_type:
-        
+
         if chapter_type == 'MKV':
             Trims2ts = [(fmt_time(i[0]),fmt_time(i[1])) for i in Trims2ts]
 
@@ -171,7 +177,6 @@ def main(args):
             from templates import AutoMKVChapters as amkvc
             output = o.chapters[:-4] if not o.test else None
             chaps = amkvc(o.template,output=output,avs=a[0],trims=Trims2ts,kframes=Trims2,uid=o.uid)
-            
 
         else:
             # Assign names to each chapter if --chnames
@@ -312,7 +317,7 @@ def convert_v1_to_v2(v1,max,asm,v2=None,first=0):
         with open(v2,'wb') as v2f:
             from os import linesep as ls
             header = [en('# timecode format v2'+ls)] if first == 0 else [b'']
-            v2f.writelines(header+[en(('%3.6f' % s)+ls ) for s in o[first:]])
+            v2f.writelines(header + [en(('{0:3.6f}'.format(s)) + ls ) for s in o[first:]])
     return o[first:]
 
 def parse_tc(tcfile, max=0, otc=None,first=0):
@@ -335,7 +340,7 @@ def parse_tc(tcfile, max=0, otc=None,first=0):
         timecodes = Fraction(num,den)
         if otc:
             convert_v1_to_v2([],max+2,timecodes,otc,first)
-    
+
     else:
         type = 'vfr'
         with open(tcfile) as tc:
@@ -348,7 +353,7 @@ def parse_tc(tcfile, max=0, otc=None,first=0):
             asm = ret[1] if len(ret) == 2 else exit('there is no assumed fps')
             if v1:
                 ret = convert_v1_to_v2(v1,max,asm,otc,first)
-                timecodes = ['%3.6f\n' % i for i in ret]
+                timecodes = ['{0:3.6f}\n'.format(i) for i in ret]
             else:
                 timecodes = correct_to_ntsc(asm)
                 type = 'cfr'
@@ -365,7 +370,7 @@ def parse_tc(tcfile, max=0, otc=None,first=0):
                 fps = correct_to_ntsc(Fraction.from_float(sample / average * 1000))
                 ret = convert_v1_to_v2([],max,fps,first=temp_max)
                 if v1[-1][-1] is not '\n': v1[-1] += '\n'
-                v1 += ['%3.6f\n' % i for i in ret]
+                v1 += ['{0:3.6f}\n'.format(i) for i in ret]
             timecodes = v1
 
     return (timecodes, type), max
@@ -483,7 +488,7 @@ def parse_avs(avs, label=None):
 
     with open(avs) as avsfile:
         avs = avsfile.readlines()
-        findTrims = compile("(?<!#)[^#]*\s*\.?\s*%s\((\d+)\s*,\s*(\d+)\)%s" % (label if label else "trim","" if label else "(?i)"))
+        findTrims = compile("(?<!#)[^#]*\s*\.?\s*{0}\((\d+)\s*,\s*(\d+)\){1}".format(label if label else "trim", "" if label else "(?i)"))
         for line in avs:
             if findTrims.match(line):
                 Trims = trimre.findall(line)
@@ -550,7 +555,7 @@ def parse_trims(avs, fps, outfps=None, otc=None, input=None, label=None):
             lastts = get_ts(last+1,tc)
             adjacent = True if not fn1-(last+1) else False
             offset += fn1-(last+1)
-            offsetts += 0 if adjacent else fn1ts-lastts           
+            offsetts += 0 if adjacent else fn1ts-lastts
 
         if input:
             # Make list with timecodes to cut audio
@@ -580,12 +585,12 @@ def parse_trims(avs, fps, outfps=None, otc=None, input=None, label=None):
 
 def write_qpfile(qpfile,trims):
     """Simply writes keyframes for use in x264 from a list of Trims."""
-    
+
     with open(qpfile, "w") as qpf:
         if trims[0][0] == 0:
             del trims[0]
         for trim in trims:
-            qpf.write('%s K\n' % trim[0])
+            qpf.write('{0} K\n'.format(trim[0]))
 
 if __name__ == '__main__':
     main(argv[1:])

templates.py
@@ -19,7 +19,7 @@ class AutoMKVChapters:
             chf = open(chapfile+'.xml','w',encoding='utf-8')
             head = '<?xml version="1.0" encoding="UTF-8"?>\n<!-- <!DOCTYPE Tags SYSTEM "matroskatags.dtd"> -->\n'
             chf.write(head+'<Chapters>\n')
-            
+
             if self.num_editions > 1:
                 tagf = open(chapfile+'tags.xml','w')
                 tagf.write(head+'<Tags>\n')
@@ -28,21 +28,21 @@ class AutoMKVChapters:
 
             for ed in self.editions:
                 chf.write('\t<EditionEntry>\n')
-                chf.write('\t\t<EditionFlagHidden>%d</EditionFlagHidden>\n' % ed.hidden if ed.hidden else '')
-                chf.write('\t\t<EditionFlagDefault>%d</EditionFlagDefault>\n' % ed.default if ed.default else '')
-                chf.write('\t\t<EditionFlagOrdered>%d</EditionFlagOrdered>\n' % ed.ordered if ed.ordered else '')
-                chf.write('\t\t<EditionUID>%d</EditionUID>\n' % ed.uid)
-                
+                chf.write('\t\t<EditionFlagHidden>{0:d}</EditionFlagHidden>\n'.format(ed.hidden) if ed.hidden else '')
+                chf.write('\t\t<EditionFlagDefault>{0:d}</EditionFlagDefault>\n'.format(ed.default) if ed.default else '')
+                chf.write('\t\t<EditionFlagOrdered>{0:d}</EditionFlagOrdered>\n'.format(ed.ordered) if ed.ordered else '')
+                chf.write('\t\t<EditionUID>{0:d}</EditionUID>\n'.format(ed.uid))
+
                 if tagf:
                     tagf.write('\t<Tag>\n\t\t<Targets>\n')
-                    tagf.write('\t\t\t<EditionUID>%d</EditionUID>\n' % ed.uid)
+                    tagf.write('\t\t\t<EditionUID>{0:d}</EditionUID>\n'.format(ed.uid))
                     tagf.write('\t\t\t<TargetTypeValue>50</TargetTypeValue>\n\t\t</Targets>\n')
                     num_names = len(ed.name) if len(ed.name) < len(self.lang) else len(self.lang)
                     for i in range(num_names):
                         tagf.write('\t\t<Simple>\n\t\t\t<Name>TITLE</Name>\n')
-                        tagf.write('\t\t\t<String>%s</String>\n' % (ed.name[i] if ed.name[i] != '' else ed.name[i-1]))
-                        tagf.write('\t\t\t<TagLanguage>%s</TagLanguage>\n' % self.lang[i])
-                        tagf.write('\t\t\t<DefaultLanguage>%d</DefaultLanguage>\n' % (1 if i == 0 else 0))
+                        tagf.write('\t\t\t<String>{0}</String>\n'.format(ed.name[i] if ed.name[i] != '' else ed.name[i-1]))
+                        tagf.write('\t\t\t<TagLanguage>{0}</TagLanguage>\n'.format(self.lang[i]))
+                        tagf.write('\t\t\t<DefaultLanguage>{0:d}</DefaultLanguage>\n'.format(1 if i == 0 else 0))
                         tagf.write('\t\t</Simple>\n')
                     tagf.write('\t</Tag>\n')
 
@@ -51,18 +51,18 @@ class AutoMKVChapters:
                     num_names = len(ch.name) if len(ch.name) < len(self.lang) else len(self.lang)
                     for i in range(num_names):
                         chf.write('\t\t\t<ChapterDisplay>\n')
-                        chf.write('\t\t\t\t<ChapterString>%s</ChapterString>\n' % (ch.name[i] if ch.name[i] != '' else ch.name[i-1]))
-                        chf.write('\t\t\t\t<ChapterLanguage>%s</ChapterLanguage>\n' % self.lang[i] if self.lang[i] != 'eng' else '')
-                        chf.write('\t\t\t\t<ChapterCountry>%s</ChapterCountry>\n' % self.country[i] if i < len(self.country) else '')
+                        chf.write('\t\t\t\t<ChapterString>{0}</ChapterString>\n'.format(ch.name[i] if ch.name[i] != '' else ch.name[i-1]))
+                        chf.write('\t\t\t\t<ChapterLanguage>{0}</ChapterLanguage>\n'.format(self.lang[i]) if self.lang[i] != 'eng' else '')
+                        chf.write('\t\t\t\t<ChapterCountry>{0}</ChapterCountry>\n'.format(self.country[i]) if i < len(self.country) else '')
                         chf.write('\t\t\t</ChapterDisplay>\n')
-                    chf.write('\t\t\t<ChapterUID>%d</ChapterUID>\n' % ch.uid)
-                    chf.write('\t\t\t<ChapterTimeStart>%s</ChapterTimeStart>\n' % ch.start)
-                    chf.write('\t\t\t<ChapterTimeEnd>%s</ChapterTimeEnd>\n' % ch.end)
-                    chf.write('\t\t\t<ChapterFlagHidden>%d</ChapterFlagHidden>\n' % ch.hidden if ch.hidden != 0 else '')
-                    chf.write('\t\t\t<ChapterFlagEnabled>%d</ChapterFlagEnabled>\n' % ch.enabled if ch.enabled != 1 else '')
-                    chf.write('\t\t\t<ChapterSegmentUID format="hex">%s</ChapterSegmentUID>\n' % ch.suid if ch.suid else '')
+                    chf.write('\t\t\t<ChapterUID>{0:d}</ChapterUID>\n'.format(ch.uid))
+                    chf.write('\t\t\t<ChapterTimeStart>{0}</ChapterTimeStart>\n'.format(ch.start))
+                    chf.write('\t\t\t<ChapterTimeEnd>{0}</ChapterTimeEnd>\n'.format(ch.end))
+                    chf.write('\t\t\t<ChapterFlagHidden>{0:d}</ChapterFlagHidden>\n'.format(ch.hidden) if ch.hidden != 0 else '')
+                    chf.write('\t\t\t<ChapterFlagEnabled>{0:d}</ChapterFlagEnabled>\n'.format(ch.enabled) if ch.enabled != 1 else '')
+                    chf.write('\t\t\t<ChapterSegmentUID format="hex">{0}</ChapterSegmentUID>\n'.format(ch.suid) if ch.suid else '')
                     chf.write('\t\t</ChapterAtom>\n')
-                
+
                 chf.write('\t</EditionEntry>\n')
 
             chf.write('</Chapters>\n')
@@ -92,11 +92,11 @@ class AutoMKVChapters:
 
             # compensate for amkvc's fps assumption
             if self.fps in ('24','30'):
-                fps = '%s/1.001' % self.fps
+                fps = self.fps + '/1.001'
             else:
                 self.fps = str(fps)
             if self.ofps and self.ofps in ('24','30'):
-                ofps = '%s/1.001' % self.ofps
+                ofps = self.ofps + '/1.001'
             else:
                 ofps = str(self.ofps)
             Trims2, Trims2ts = parse_trims(avs, fps, ofps)[2:4]
@@ -178,8 +178,8 @@ class AutoMKVChapters:
             ed.num = i+1
             ed.chapters = []
             stuff = {}
-            
-            for k, v in config.items('edition%d' % ed.num):
+
+            for k, v in config.items('edition{0:d}'.format(ed.num)):
                 if k == 'default':
                     ed.default = int(v)
                 elif k == 'name':
@@ -205,7 +205,7 @@ class AutoMKVChapters:
                 cuid += 1
                 ch.uid = cuid
                 ch.num = j+1
-                
+
                 for k, v in stuff[j+1]:
                     if k == 'name':
                         ch.name = v.split(',')
@@ -227,7 +227,7 @@ class AutoMKVChapters:
                 elif ch.suid:
                     from os.path import isfile
                     from subprocess import check_output
-                    
+
                     suid_re = compile('^\| \+ Segment UID: (.*)(?m)')
                     duration_re = compile('^\| \+ Duration: \d+\.\d*s \((\d+:\d+:\d+.\d+)\)(?m)')
                     suid = None

test/test.py
@@ -3,7 +3,7 @@
 from subprocess import check_output,CalledProcessError
 from re import search
 from os import rename, unlink
-from os.path import isfile
+from os.path import isfile, join as pjoin
 
 args = [
         r'-i audio.flac -vf 24000/1001 test.avs --test',
@@ -21,11 +21,11 @@ args = [
 stable = check_output('git tag',shell=True).decode()[:-1].split('\n')[-1]
 current = search('^\* (\w+)(?m)',check_output("git branch",shell=True).decode()[:-1]).group(1)
 
-check_output('git show %s:vfr.py > vfr.py' % stable,shell=True)
-check_output('git show %s:templates.py > templates.py' % stable,shell=True)
+check_output('git show {0}:vfr.py > vfr.py'.format(stable), shell=True)
+check_output('git show {0}:templates.py > templates.py'.format(stable), shell=True)
 try:
-    old = [check_output(r'python vfr.py %s' % command.format('old'),shell=True) for command in args]
-    new = [check_output(r'python ..\vfr.py %s' % command.format('new'),shell=True) for command in args]
+    old = [check_output(r'python3 vfr.py ' + command.format('old'), shell=True) for command in args]
+    new = [check_output(r'python3 {0} {1}'.format(pjoin('..', 'vfr.py'), command.format('new')), shell=True) for command in args]
     fails = []
     for i in range(len(old)):
         if old[i] != new[i]:
@@ -37,7 +37,7 @@ try:
                 old = oldf.readlines()
                 new = newf.readlines()
                 if old != new:
-                    fails.append('%s and %s are not identical.' % (f[0],f[1]))
+                    fails.append('{0} and {1} are not identical.'.format(f[0],f[1]))
     if len(fails) != 0:
         print('Failed:')
         [print(i) for i in fails]

commit fd5592e934c6044ce4db2803c7fa14d04a3bf42d

Fix regression on cutting introduced by commit 62fafe3a7
Make verbose output format of mergeCmd like cutCmd

vfr.py
@@ -121,9 +121,10 @@ def main(args):
         ret = delre.search(o.input)
         delay = '{0}:{1}'.format(tid, o.delay if o.delay else ret.group(1)) if o.delay or ret else None
 
-        cutCmd = [mkvmerge, '-o', o.output + '.split.mka', o.input, '--split', 'timecodes:' + cuttimes]
+        cutCmd = [mkvmerge, '-o', o.output + '.split.mka']
         if delay: cutCmd.extend(['--sync', delay])
         if sbr: cutCmd.extend(['--aac-is-sbr', sbr])
+        cutCmd.extend([o.input, '--split', 'timecodes:' + cuttimes])
         if o.verbose: print('Cutting: {0}\n'.format(' '.join(['"{0}"'.format(i) for i in cutCmd])))
         else: cutCmd.append('-q')
 
@@ -140,7 +141,7 @@ def main(args):
                     merge.append('{0}{1}.split-{2:03d}.mka'.format('+' if len(merge) > 0 else '', o.output, i))
             mergeCmd = [mkvmerge, '-o', o.output]
             mergeCmd.extend(merge)
-            if o.verbose: print('\nMerging: {0}\n'.format(mergeCmd))
+            if o.verbose: print('\nMerging: {0}\n'.format(' '.join(['"{0}"'.format(i) for i in mergeCmd])))
             else: mergeCmd.append('-q')
 
             if not o.test: